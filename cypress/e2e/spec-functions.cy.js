2ad31aaab45ad839b3400af2a25fc6e670fcb722
leakastner <133603215+leakastner@users.noreply.github.com>
2023-08-20 18:39:16 +0300

    Add files via upload

--- cypress/e2e/spec-functions.cy.js
+++ cypress/e2e/spec-functions.cy.js
@@ -1,19 +1,19 @@
 let resultText;
 
 export const getTableCellTextByXpath = async (WebElementTable, searchText, returnColumnText, expectedText) => {
-  //get element by cypress options (as xpath) and find the the text 
+  //Get element by cypress options (as xpath) and find the the text 
   cy.get('#customers>tbody>tr').find("td").contains(searchText).then(($td) => {
     const $tr = $td.parent()
     cy.get($tr).find("td").eq(returnColumnText).then((text2) => {
       resultText = text2.text();
 
-      //the sent to verifyTableCellTextByText function with 2 params: result text and expected result text
+      //The sent to verifyTableCellTextByText function with 2 params: result text and expected result text
       try {
         verifyTableCellTextByText(resultText, expectedText);
-      } catch (error) {
+      }
+      catch (err) {
         cy.log("Error: The send to verifyTableCellTextByText function failed")
       }
-
     })
   })
 };
@@ -21,11 +21,11 @@ export const getTableCellTextByXpath = async (WebElementTable, searchText, retur
 //the following function get result and expected result texts and pring the result
 export const verifyTableCellTextByText = async (resultText, expectedText) => {
   if (expectedText == resultText) {
-    cy.log("passed - expected value is:", expectedText, "the value is:", resultText)
-    console.log("passed - expected value is:", expectedText, "the value is:", resultText)
+    cy.log("Passed - expected value is:", expectedText, "the value is:", resultText)
+    console.log("Passed - expected value is:", expectedText, ", the value is:", resultText)
   }
   else
-    console.log("failed - expected value is:", expectedText, "but the value is:", resultText)
+    console.log("Failed - expected value is:", expectedText, ", but the value is:", resultText)
 };
 
 
--- cypress/e2e/spec-test.cy.js
+++ cypress/e2e/spec-test.cy.js
@@ -1,46 +1,41 @@
 import { getTableCellTextByXpath } from "./spec-functions.cy.js";
-//import {} from "./moc data arrs.js"
 
-describe('test', () => {
+describe('VerySoft Test', () => {
+  //Moc Data for tests
   let table;
-  let t;
-
-  let arrExcellPositive = ["Helen Bennett", 2, "UK", "Alfreds Futterkiste", 2, "Germany", "Roland Mendel", 0, "Ernst Handel"]
-  let arrExcellNegative = ["Helen Bennett", 2, "UKK", "Alfreds Futterkiste", 2, "G", "Roland Mendel", 0, "E Handel"]
+  //Moc = arr with correct details - for positive test
+  let arrExcellPositive = ["Helen Bennett", 2, "UK", "Alfreds Futterkiste", 2, "Germany", "Roland Mendel", 0, "Ernst Handel", "Laughing Bacchus Winecellars", 2, "Canada", "Giovanni Rovelli", 0, "Magazzini Alimentari Riuniti", "Magazzini Alimentari Riuniti", 2, "Italy"]
+  //Moc = arr with incorrect details - for negative test
+  let arrExcellNegative = ["Helen Bennett", 2, "UKK", "Alfreds Futterkiste", 2, "G", "Roland Mendel", 0, "E Handel", "Laughing Bacchus Winecellars", 2, "Cada", "Giovanni Rovelli", 0, "Magazzini Aliment", "Magazzini Alimentari Riuniti", 2, "Itay"]
 
   it('positive test', () => {
-   //log('ffff')
-  console.log('ffff');
-//ypress.log("kkk")
+    //Go to Url
     try {
       cy.visit('https://www.w3schools.com/html/html_tables.asp')
-      
-    } catch (err) { alert("Error: connection to html table failed on the Positive test") }
 
+    } catch (err) { alert("Error: connection to html table failed on the Positive test") }
+    //The pollowing loop run on all the moc data positive arr and send the values for tests to the getTableCellTextByXpath function
     try {
       for (let i = 0; i < arrExcellPositive.length - 1; i += 3) {
-        getTableCellTextByXpath(t, arrExcellPositive[i], arrExcellPositive[i + 1], arrExcellPositive[i + 2]);
+        getTableCellTextByXpath(table, arrExcellPositive[i], arrExcellPositive[i + 1], arrExcellPositive[i + 2]);
       }
-    } catch (error) { cy.log("Error: the sent to getTableCellTextByXpath function fails on the Positive test") }
+    } catch (err) { cy.log("Error: the sent to getTableCellTextByXpath function fails on the Positive test") }
 
-    //})
   })
 
   it('negative test', () => {
+    //Go to Url
     try {
       cy.visit('https://www.w3schools.com/html/html_tables.asp')
-    } catch (error) { cy.console.log("Error: connection to html table failed on the Negative test") }
+    } catch (err) { cy.console.log("Error: connection to html table failed on the Negative test") }
+    //The pollowing loop run on all the moc data positive arr and send the values for tests to the getTableCellTextByXpath function
     try {
       for (let i = 0; i < arrExcellNegative.length - 1; i += 3) {
-        getTableCellTextByXpath(t, arrExcellNegative[i], arrExcellNegative[i + 1], arrExcellNegative[i + 2]);
+        getTableCellTextByXpath(table, arrExcellNegative[i], arrExcellNegative[i + 1], arrExcellNegative[i + 2]);
       }
-    } catch (error) {
+    } catch (err) {
       cy.log("Error:  the sent to getTableCellTextByXpath function fails on the Negative test")
       console.log("Error:  the sent to getTableCellTextByXpath function fails on the Negative test")
     }
   })
 })
-   // cy.get('#customers').then((table) => {
-    //   cy.log(table)
-    //   t=table.id();
-    // });
